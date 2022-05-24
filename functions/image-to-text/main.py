70fe76f122e63624a77fca2b7d5182ac23b21d33
Peppe3009 <44870078+Peppe3009@users.noreply.github.com>
2022-05-25 00:14:28 +0200

    Fanculo

--- functions/image-to-text/main.py
+++ functions/image-to-text/main.py
@@ -1,14 +1,31 @@
+import base64, io, cv2
+import numpy as np
+from PIL import Image, ImageFilter 
 import functions_framework
 
-CLOUD = False
-
 @functions_framework.http
-def image_to_text(request):
-    request_body = request.json
-    bpm = request_body["bpm"]
-    image_b64 = request_body["image"]
-    image = rebuild_image(image_b64)
-    return "Hello World!", 200
+def hello(request):
+    im_b64 = request.json['image']
+    img_bytes = base64.b64decode(im_b64.encode('utf-8'))
+    img_for_prediction = pre_processing(img_bytes, HEIGHT)
+    
+    return {'greeting' : "Ciao TOTORE"}
 
-def rebuild_image(image_b64):
-    return
+def pre_processing(file, height):
+    image = Image.open(io.BytesIO(file)).convert('L')
+    image = image.filter(ImageFilter.SHARPEN)
+    image = np.array(image)
+    image = resize(image, height)
+    image = normalize(image)
+
+    image = np.asarray(image).reshape(1, image.shape[0], image.shape[1], 1)
+    return image
+
+def normalize(image):
+    return (255. - image) / 255.
+
+
+def resize(image, height):
+    width = int(float(height * image.shape[1]) / image.shape[0])
+    sample_img = cv2.resize(image, (width, height))
+    return sample_img
