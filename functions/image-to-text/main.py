35449430f09612a6bf1dadf6e8fd8fc55fb3e489
AntonioWar <antonioguerra.it@gmail.com>
2022-05-24 23:40:28 +0200

    feat: create rebuild image function draft

--- functions/image-to-text/main.py
+++ functions/image-to-text/main.py
@@ -7,4 +7,8 @@ def image_to_text(request):
     request_body = request.json
     bpm = request_body["bpm"]
     image_b64 = request_body["image"]
+    image = rebuild_image(image_b64)
     return "Hello World!", 200
+
+def rebuild_image(image_b64):
+    return
