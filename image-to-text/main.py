fc566356b485d7807d8e858ce3a76af52eea86c4
AntonioWar <antonioguerra.it@gmail.com>
2022-05-26 21:11:43 +0200

    feat: image-to-text version locally tested

--- image-to-text/main.py
+++ image-to-text/main.py
@@ -1,16 +1,16 @@
-import base64, io, cv2
+import base64, io
 import numpy as np
-from PIL import Image, ImageFilter
-import tensorflow as tf
+from PIL import Image, ImageFilter 
 import functions_framework
+import tensorflow as tf
 
 @functions_framework.http
 def image_to_text(request):
     im_b64 = request.json['image']
     img_bytes = base64.b64decode(im_b64.encode('utf-8'))
-    model = "semantic_model.meta"
+    model = "model/semantic_model.meta"
     voc_file = "vocabulary_semantic.txt"
-
+    
     tf.compat.v1.reset_default_graph()
     sess = tf.compat.v1.InteractiveSession()
     tf.compat.v1.disable_eager_execution()
@@ -25,38 +25,39 @@ def image_to_text(request):
     width_reduction_tensor = graph.get_tensor_by_name("width_reduction:0")
     logits = tf.compat.v1.get_collection("logits")[0]
 
-    print(f"seq_len {seq_len}, rnn_keep_prob {rnn_keep_prob}")
+    
     WIDTH_REDUCTION, HEIGHT = sess.run([width_reduction_tensor, height_tensor])
     decoded, _ = tf.nn.ctc_greedy_decoder(logits, seq_len)
 
     image_for_prediction = pre_processing(img_bytes, HEIGHT)
     seq_lenghts = [image_for_prediction.shape[2] / WIDTH_REDUCTION]
-    prediction = sess.run(decoded, feed_dict={input: image_for_prediction, seq_len: seq_lenghts, rnn_keep_prob: 1.0})
+    prediction = sess.run(decoded, feed_dict={input:image_for_prediction, seq_len: seq_lenghts, rnn_keep_prob: 1.0})
     str_predictions = sparse_tensor_to_strs(prediction)
     array_of_notes = from_prediction_to_note(str_predictions, voc_file)
+
+    sess.close()
     if array_of_notes is not None:
-        return {'array_of_notes': array_of_notes}, 200
+        return {'notes' : array_of_notes}
     else:
         return "", 500
 
-
 def pre_processing(file, height):
+     
     image = Image.open(io.BytesIO(file)).convert('L')
     image = image.filter(ImageFilter.SHARPEN)
-    image = np.array(image)
     image = resize(image, height)
+    image = np.array(image)
     image = normalize(image)
+
     image = np.asarray(image).reshape(1, image.shape[0], image.shape[1], 1)
     return image
-
 
 def normalize(image):
     return (255. - image) / 255.
 
-
 def resize(image, height):
-    width = int(float(height * image.shape[1]) / image.shape[0])
-    sample_img = cv2.resize(image, (width, height))
+    width = int(float(height * image.size[0]) / image.size[1])
+    sample_img = image.resize((width, height), resample=0)
     return sample_img
 
 
@@ -77,7 +78,6 @@ def sparse_tensor_to_strs(sparse_tensor):
         ptr = ptr + 1
     strs[b] = string
     return strs
-
 
 def from_prediction_to_note(str_predictions, voc_file):
     array_of_notes = []
